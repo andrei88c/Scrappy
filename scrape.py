09a4324565024d7c312ad59ec8c594ebb409c2fc
Andrei <andreic88@gmail.com>
2013-09-14 09:35:47 +0300

    3 Radios

--- scrape.py
+++ scrape.py
@@ -1,7 +1,15 @@
 import urllib2, datetime
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 
 soup  = BeautifulSoup(urllib2.urlopen("http://www.vibefm.ro/live/").read())
 print datetime.datetime.utcnow()
-print soup('div',{'id': 'live-current-track'})[0].a()[0].string
+print soup('a',{'title': 'Asculta live'})[0].string
+
+soup  = BeautifulSoup(urllib2.urlopen("http://www.radiozu.ro/").read())
+print datetime.datetime.utcnow()
+print  soup('div',{'class': 'piesa-live'})[0].get_text(' - ',strip=True)
+
+soup  = BeautifulSoup(urllib2.urlopen("http://www.kissfm.ro/").read())
+print datetime.datetime.utcnow()
+print  soup('h6',{})[0].get_text(' - ',strip=True)
